fde992bcfc171e871c2ba6e4fa36a6e554379f8d
Mikita Shchyhelski <m.shchyhelski@extern.evana.de>
2023-08-08 14:38:04 +0200

    add support for quoted-printable for contentDecode

--- parsemail.go
+++ parsemail.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"mime"
 	"mime/multipart"
+	"mime/quotedprintable"
 	"net/mail"
 	"strings"
 	"time"
@@ -45,9 +46,11 @@ func Parse(r io.Reader) (email Email, err error) {
 	case contentTypeMultipartRelated:
 		email.TextBody, email.HTMLBody, email.EmbeddedFiles, err = parseMultipartRelated(msg.Body, params["boundary"])
 	case contentTypeTextPlain:
+		msg.Body, _ = decodeContent(msg.Body, msg.Header.Get("Content-Transfer-Encoding"))
 		message, _ := ioutil.ReadAll(msg.Body)
 		email.TextBody = strings.TrimSuffix(string(message[:]), "\n")
 	case contentTypeTextHtml:
+		msg.Body, _ = decodeContent(msg.Body, msg.Header.Get("Content-Transfer-Encoding"))
 		message, _ := ioutil.ReadAll(msg.Body)
 		email.HTMLBody = strings.TrimSuffix(string(message[:]), "\n")
 	default:
@@ -362,8 +365,9 @@ func decodeContent(content io.Reader, encoding string) (io.Reader, error) {
 		if err != nil {
 			return nil, err
 		}
-
 		return bytes.NewReader(dd), nil
+	case "quoted-printable":
+		return quotedprintable.NewReader(content), nil
 	case "":
 		return content, nil
 	default:
